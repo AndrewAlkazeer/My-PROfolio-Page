02558f813b7a020f38dfb65bfde6228fdb58c9fa
Andrew Alkazeer <ferasalkzer@hotmail.com>
2019-06-25 13:29:04 -0600

    removed nested folder from my pc plus changed the meta code for mobile

--- jQuery.js
+++ jQuery.js
@@ -3,39 +3,62 @@ $(document).ready(()=>{
 
 $('.project-1-absolute').on('click', ()=>{
 
-$('.project-1-hidden').css({display: 'block', transition: '2s'});
+$('.project-1-hidden').css({display: 'block'});
 
 });
 
 $('.project-1-hidden-div').on('click', ()=>{
 
-    $('.project-1-hidden').css({display: 'none', transition: '2s'});
+    $('.project-1-hidden').css({display: 'none'});
 
 });
 
 $('.project-2-absolute').on('click', ()=>{
 
-    $('.project-2-hidden').css({display: 'block', transition: '2s'});
+    $('.project-2-hidden').css({display: 'block'});
     
     });
     
     $('.project-2-hidden-div').on('click', ()=>{
     
-        $('.project-2-hidden').css({display: 'none', transition: '2s'});
+        $('.project-2-hidden').css({display: 'none'});
     
     });
 
     $('.project-3-absolute').on('click', ()=>{
 
-        $('.project-3-hidden').css({display: 'block', transition: '2s'});
+        $('.project-3-hidden').css({display: 'block'});
         
         });
         
         $('.project-3-hidden-div').on('click', ()=>{
         
-            $('.project-3-hidden').css({display: 'none', transition: '2s'});
+            $('.project-3-hidden').css({display: 'none'});
         
         });
+
+  // Testing Here
+
+
+        $('.project-3-absolute').on('click', ()=>{
+
+            $('.project-3-hidden').animate({
+                opactiy: 0.25,
+                height: 'toggle',
+                left: '50'
+            }, 5000, ()=>{
+                
+            });
+            
+            });
+            
+            $('.project-3-hidden-div').on('click', ()=>{
+            
+                $('.project-3-hidden').css({display: 'none'});
+            
+            });
+
+ // Testing Here
 
 
 });
