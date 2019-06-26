99b7ea47e326eb200dff51b1ec0aa71a98b61f7c
Andrew Alkazeer <ferasalkzer@hotmail.com>
2019-06-26 02:59:29 -0600

    a couple animation added to first page but nav on mobile crashed waiting to fix for now

--- jQuery.js
+++ jQuery.js
@@ -24,7 +24,7 @@ $('.project-2-absolute').on('click', ()=>{
         $('.project-2-hidden').css({display: 'none'});
     
     });
-
+/*
     $('.project-3-absolute').on('click', ()=>{
 
         $('.project-3-hidden').css({display: 'block'});
@@ -36,25 +36,33 @@ $('.project-2-absolute').on('click', ()=>{
             $('.project-3-hidden').css({display: 'none'});
         
         });
+*/
+
+$(window).on('load', function(){
+
+   $(".first-page-container-div-1").delay(200).slideDown('slow');
+   $(".first-page-container-div-2").delay(500).slideDown('slow');
+   $(".first-page-container-div-3").delay(800).slideDown('slow');
+   $(".first-page-container-div-4").delay(1100).slideDown('slow');
+   $('.first-page-h1').delay(1500).animate({left: '60%'}, 500).animate({left: '50%'}, 1000);
+
+
+});
+
 
   // Testing Here
 
 
         $('.project-3-absolute').on('click', ()=>{
 
-            $('.project-3-hidden').animate({
-                opactiy: 0.25,
-                height: 'toggle',
-                left: '50'
-            }, 5000, ()=>{
+            $('.project-3-hidden').fadeToggle( "slow", "linear" );
                 
             });
             
-            });
             
             $('.project-3-hidden-div').on('click', ()=>{
             
-                $('.project-3-hidden').css({display: 'none'});
+                $('.project-3-hidden').fadeToggle( "slow", "linear" );
             
             });
 
